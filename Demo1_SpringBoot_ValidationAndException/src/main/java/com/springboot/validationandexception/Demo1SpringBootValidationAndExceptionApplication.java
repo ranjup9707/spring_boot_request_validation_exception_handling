6fd88bb733e8842a0438da56601726fee1a720c1
Ranju <ranjupillai2016@gmail.com>
2023-07-12 18:56:49 +0530

    Initial commit

---
+++ Demo1_SpringBoot_ValidationAndException/src/main/java/com/springboot/validationandexception/Demo1SpringBootValidationAndExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.validationandexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1SpringBootValidationAndExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1SpringBootValidationAndExceptionApplication.class, args);
+	}
+
+}
---
+++ Demo1_SpringBoot_ValidationAndException/src/test/java/com/springboot/validationandexception/Demo1SpringBootValidationAndExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.validationandexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1SpringBootValidationAndExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
